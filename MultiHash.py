062ec82a3ad36579e08bef7034edc4f88cf9c25f
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2023-07-20 11:40:57 -0500

    Update MultiHash.py to v2.0
    * Updated to output all hashes instead of only one to improve ease of use
    * Cleaned up code

--- MultiHash.py
+++ MultiHash.py
@@ -17,37 +17,25 @@ print(C("""
 
 """).rainbow(rgb_mode=True))
 
-print(color("Current version: Multi-Hash v1.1", fore='orange', style='bright'))
+print(color("Current version: Multi-Hash v2.0", fore='orange', style='bright'))
 
 print(color("""
-Algorithm List
+Supported Algorithms
 --------------
-1: md5 Hash
-2: sha256 Hash
-3: sha512 Hash
+1: md5
+2: sha256
+3: sha512
 --------------
 """, fore='green', style='bright'))
 
 def main():
     file_name = str(input("Enter file path: "))
-    method = int(input("Enter hashing algorithm(1-3): "))
-    algorithm(file_name, method)
-
-def algorithm(file_name, method):
-    if method == 1:
-        md5(file_name)
-        
-    elif method == 2:
-        sha256(file_name)
-
-    elif method == 3:
-        sha512(file_name)
-    else:
-        print(color("Please select valid method.", fore='red', style='bright'))
-        main()
+    md5(file_name)
+    sha256(file_name)
+    sha512(file_name)
+    reup()
 
 def md5(file_name):
-    print("Hashing with md5")
     BLOCKSIZE = 65536
     md5 = hashlib.md5()
     with open(file_name, 'rb') as afile:
@@ -55,11 +43,9 @@ def md5(file_name):
         while len(buf) > 0:
             md5.update(buf)
             buf = afile.read(BLOCKSIZE)
-    print("Your md5 hash is: " + color(md5.hexdigest(), fore='green'))
-    reup()
+    print("md5     : ", color(md5.hexdigest(), fore='green'))
 
 def sha256(file_name):
-    print("Hashing with sha256")
     BLOCKSIZE = 65536
     sha256 = hashlib.sha256()
     with open(file_name, 'rb') as afile:
@@ -67,11 +53,9 @@ def sha256(file_name):
         while len(buf) > 0:
             sha256.update(buf)
             buf = afile.read(BLOCKSIZE)
-    print("Your sha256 hash is: " + color(sha256.hexdigest(), fore='green'))
-    reup()
+    print("sha256  : ", color(sha256.hexdigest(), fore='green'))
 
 def sha512(file_name):
-    print("Hashing with sha512")
     BLOCKSIZE = 65536
     sha512 = hashlib.sha512()
     with open(file_name, 'rb') as afile:
@@ -79,16 +63,13 @@ def sha512(file_name):
         while len(buf) > 0:
             sha512.update(buf)
             buf = afile.read(BLOCKSIZE)
-    print("Your sha512 hash is: " + color(sha512.hexdigest(), fore='green'))
-    reup()
+    print("sha512  : ", color(sha512.hexdigest(), fore='green'))
 
 def reup():
     reup = input("Would you like to hash something else? (y or n): ")
     if reup.upper() == "Y":
         main()
     elif reup.upper() == "N":
-        print(color("Thank you for using Multi-Hash! Come back soon!", fore='blue', style='bright'))
-        time.sleep(2)
         exit()
         
 main()
